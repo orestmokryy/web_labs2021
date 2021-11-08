dbbbf818ab054e26dac6467ef820541f78c3279e
BrigYeet <orest.mokryy@gmail.com>
2021-11-08 16:12:14 +0200

    Added lading pages for lab 1-2.

---
+++ lab1-2/js/toggler.js
@@ -0,0 +1,13 @@
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+
+
+toggle.addEventListener("click", function(){
+    if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+    toggle.querySelector("a").innerHTML = "<i class='fa fa-bars'></i>";
+  } else {
+    menu.classList.add("active");
+    toggle.querySelector("a").innerHTML = "<i class='fa fa-times'></i>";
+  }
+});
